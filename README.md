3e73ced7e044dedcf73ed0ee37e0503d6f43e4a9
etene <etienne.noss@gmail.com>
2013-05-23 15:08:24 +0200

    code refactor, readme improvements

--- README.md
+++ README.md
@@ -1,27 +1,38 @@
 # Random Sentence Generator
 
-This script takes one or more input files or stdin and generates random sentences based on the input.
+This script takes one or more input files (or standard input) and generates random sentences based on it.
+It uses some sort of Markov Chains algorithm where each pair of words is associated with its possible successors.
 
-## Usage
+### Usage
 ```
-python rsg.py file.txt file2.txt
-```
-or
-```
-python rsg.py < file.txt
-```
-or
-```
-some_text_generating_command | python rsg.py
+python rsg.py [input file(s)] [-n minimum word count]
 ```
 
-## Requirements
+### Example
+*Using the King James Version bible as input*
+```
+$python rsg.py kjv12.txt -n 20
+Like cheese? Thou hast slain him with your speeches. They shall eat, and went his way rejoicing.
+```
+
+*Using the 1598 French Constitution*
+```
+$ python rsg.py const.txt
+La délégation de vote des assemblées parlementaires, des séances supplémentaires pour permettre, le crédit et les modalités prévues par une loi organique votée dans les mêmes termes par les lois peuvent être modifiés par décret les crédits se rapportant aux services votés.
+```
+
+### Requirements
 * Python 2.5+
 * A long text
 
-## Where to find lots of text
-* The King Jame's bible is included, it's a good start
+### Where to find long text files
+* The King James Version bible is included, it's a good start
 * [Project Gutemberg](http://www.gutenberg.org/) has a lot of cool ebooks downloadable in text format
 
-## License
+### Flaws
+* Doesn't handle special characters very well
+* Doesn't retain the words' case
+
+
+### License
 See COPYING
--- rsg.py
+++ rsg.py
@@ -1,44 +1,65 @@
 #!/bin/env python
 # -*- coding: utf8 -*-
-"""
-	Random Sentence Generator
-	Generates a random sentence based on the input file(s)
-"""
-#~ Copyright © 2000 Etienne Noss <etienne.noss@etu.unistra.fr>
+"""Random Sentence Generator -- Generates random sentences (duh)"""
+
+#~ Copyright © 2013 Etienne Noss <etienne.noss@etu.unistra.fr>
 #~ This work is free. You can redistribute it and/or modify it under the
 #~ terms of the Do What The Fuck You Want To Public License, Version 2,
 #~ as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+
 
 # Imports
 import re
 import random
 import fileinput
+import sys
 
 
 # Constants
-SENTENCE_ENDS = "!?."
-SEPARATORS = ",/\.;"
-SPACELESS = "'-"
+SENTENCE_ENDS = "!?."	# Characters that may end a sentence
+SEPARATORS = ",/\.;"	# Characters that may separate words
+SPACELESS = "'-"		# Characters that must'nt be surrounded with spaces
 SPECIALCHARS = SENTENCE_ENDS + SEPARATORS + SPACELESS
+DEFAULT_MIN_WORDS = 10	# Default minimum word count
 
 
+# Functions
 def main():
 	"""I should split that into separate files"""
-	# Créée la liste de couples de mots
+	
+	if "-h" in sys.argv: # Print help and exit
+		print "usage: %s [input file(s)] [-n minimum word count]" % sys.argv[0]
+		sys.exit(0)
+	
+	min_words = DEFAULT_MIN_WORDS
+	if "-n" in sys.argv: # Parse the minimum word count
+		try:
+			min_words = int(sys.argv.pop(sys.argv.index("-n")+1))
+		except IndexError:
+			print "Option -n requires an argument (the minimum word count)."
+		except ValueError:
+			print "Invalid argument to -n (give it the minimum word count)."
+		sys.argv.remove("-n")
+	
+	# Create the word dictionnary
 	tokens = parse_file()
-	# Génère aléatoirement une suite de mots
-	seq = get_random_sequence(tokens)
-	# L'affiche en tant que phrase
+	
+	try: # Generate a word sequence
+		seq = get_random_sequence(tokens, min_words)
+	except IndexError:
+		print "There seems to be an insufficient number of words in your input."
+		sys.exit(1)
+	
+	# Print it
 	print list_to_sentence(seq)
 
 
 def parse_file():
-	"""
-	Creates a dictionnary associating 2-word tuples with their
+	"""Create a dictionnary associating 2-word tuples with their
 	possible successors"""
-
+	
 	# TODO: handle other special chars
-	regex = re.compile("([A-Za-zàâêéèîïôù]+|[" + SPECIALCHARS + "])")
+	regex = re.compile("([A-Za-zàâêéèîïôù€$£&]+|[" + SPECIALCHARS + "])")
 	words = dict()
 	lastlastword, lastword = None, None
 
@@ -50,26 +71,25 @@ def parse_file():
 			# for Foo, foo and FOO
 			word = word.lower()
 			if lastword and lastlastword:
-				words.setdefault( (lastlastword, lastword), [] ).append(word)
+				words.setdefault((lastlastword, lastword), []).append(word)
 			lastlastword, lastword = lastword, word
 	return words
 
 
-def get_random_sequence(words, min_len = 3):
-	"""Generate a random sequence of words until a setence ending character
-	is found"""
+def get_random_sequence(words, min_len=50):
+	"""Generate a random sequence of words of at least min_len characters
+	until a sentence ending character is found"""
+	
 	while True:
 		(lastlastword, lastword) = random.choice(words.keys())
-		w =  words[(lastlastword, lastword)]
-		if len([a for a in w if a in SPECIALCHARS]) < len(w):
+		if lastlastword not in SPECIALCHARS:
 			break
-	result = []
+	result = [lastlastword, lastword]
 
-	# adds a random word, then a random word chosen among its successors,
-	# and so forth
+	# Adds a random word, then a random word chosen among its successors,
+	# and so forth.
 	while True:
 		word = random.choice(words[(lastlastword, lastword)])
-
 		result.append(word)
 		lastlastword, lastword = lastword, word
 		# if a sentence ending character is found, stop
@@ -77,23 +97,21 @@ def get_random_sequence(words, min_len = 3):
 			break
 	return result
 
-def titlecaseword(w):
-	return w[0].upper() + w[1:]
 
 def list_to_sentence(word_list):
 	"""Turns a word list into a more or less typographically correct sentence"""
 	
-	prev = word_list[0]
-	resulting_string = titlecaseword(prev)
-	
+	# Start the sentence with a title case word
+	resulting_string = previous_word = word_list[0].title()
 	for word in word_list[1:]:
-		if word not in SPECIALCHARS and prev not in SPACELESS:
-			# put spaces between words, except if they are not real words
+		if word not in SPECIALCHARS and previous_word not in SPACELESS:
+			# Put spaces between words, except if they are not real words
 			resulting_string += " "
-		if prev in SENTENCE_ENDS:
-			word = titlecaseword(word)
+		if previous_word in SENTENCE_ENDS:
+			# If the previous word ended a sentence, titlecase this one
+			word = word.title()
 		resulting_string += word
-		prev = word
+		previous_word = word
 	return resulting_string
 
 
