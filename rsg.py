99f62d5921f27f6bb6310477bc0b9db7a0186cae
Étienne Noss <etienne.noss@gmail.com>
2016-12-17 21:58:25 +0100

    Can now load and save state from disk to save time

--- rsg.py
+++ rsg.py
@@ -12,6 +12,7 @@ from re import Scanner as ReScanner, UNICODE
 from collections import Counter, deque
 import random
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, FileType
+import pickle
 
 
 class Token(object):
@@ -60,6 +61,7 @@ class SentenceEnd(Punctuation):
     """A punctuation character which marks the end of a sentence"""
     RE = r"[!?\.]+"
 
+
 # A list of the token types defined above
 TOKEN_TYPES = [Word, Punctuation, SpaceLessPunctuation, SentenceEnd]
 
@@ -84,8 +86,8 @@ class RandomSentenceGenerator(object):
     this could be one of the hash table's entries :
     ('hello', 'every'): {'body': 3, 'one': 1}
 
-    The get_tokens method can then be called to get a semi random succession
-    of tokens.
+    The get_tokens method can then be called to get a semi random
+    succession of tokens.
     """
 
     KEY_SIZE = 2
@@ -95,9 +97,17 @@ class RandomSentenceGenerator(object):
                       UNICODE)
 
     def __init__(self):
-        self.data = {}
+        # Every function that modifies self._data
+        # must call _update_key_cache() afterwards
+        self._data = {}
         self._token_cache = {}
         self._key_cache = []
+
+    def _update_key_cache(self):
+        """Call when you're done modifying self._data.
+        We store key cache so we don't have to call self._data.keys()
+        (which is expensive) every time we need its keys."""
+        self._key_cache = list(self._data.keys())
 
     def feed(self, iterable):
         """Feed an iterable (yielding lines of text)"""
@@ -113,12 +123,31 @@ class RandomSentenceGenerator(object):
                 # add the current token as a possible successor to the last tokens
                 # XXX: should use a specialized counter that divides totals
                 #      by the gcd() on update
-                self.data.setdefault(tuple(last_tokens),
-                                     Counter()).update((token,))
+                self._data.setdefault(tuple(last_tokens),
+                                      Counter()).update((token,))
             last_tokens.append(token)
+        self._update_key_cache()
 
-        # update the key cache
-        self._key_cache = list(self.data.keys())
+    def restore_data(self, filep, replace=False):
+        """Restores data previously saved to disk with save_data().
+        If replace is true, replaces any existing data, else updates it.
+        """
+        loaded = pickle.load(filep)
+        if replace or not self._data:
+            self._data = loaded
+        else:
+            for key, value in loaded.items():
+                if key in self._key_cache:
+                    self._data[key].update(value)
+                    self._update_key_cache()
+                else:
+                    self._data[key] = value
+        self._update_key_cache()
+
+    def save_data(self, filep):
+        """Saves the data to a pickle-format file for later restoration.
+        """
+        pickle.dump(self._data, filep)
 
     def get_tokens(self):
         """Returns a generator that yields randomly selected tokens"""
@@ -127,7 +156,7 @@ class RandomSentenceGenerator(object):
             weighted_successors = []
             # TODO optimize this
             # lame weighted-random implementation
-            for token, weight in self.data[tuple(key)].items():
+            for token, weight in self._data[tuple(key)].items():
                 weighted_successors.extend(token for i in range(weight))
 
             yield key.popleft()
@@ -186,16 +215,41 @@ def main():
     psr = ArgumentParser(description="Generate random sentences from text",
                          formatter_class=ArgumentDefaultsHelpFormatter)
     psr.add_argument("-n", "--min-words", type=int, default=50,
-                     help="resulting sentence's minimum word count")
-    psr.add_argument("textfile", type=FileType("r"), nargs="+",
+                     help="Resulting sentence's minimum word count")
+    psr.add_argument("-q", "--quiet", default=False, action="store_true",
+                     help="Don't output anything")
+    psr.add_argument("-t", "--textfile", type=FileType("r"), nargs="+",
                      help="One or more (preferably large) text files "
                           "to use as input")
+    psr.add_argument("-l", "--load", type=FileType("rb"), metavar="FILE",
+                     help="Restore a file saved with --save")
+    psr.add_argument("-s", "--save", type=FileType("wb"), metavar="FILE",
+                     help="Save the inner state for later restoration, "
+                          "in order to avoid re-parsing text files")
+
     args = psr.parse_args()
+    if not (args.load or args.textfile):
+        psr.error("Provide at least a database file to load (-l/--load) "
+                  "or a text file (-t/--textfile).")
 
     gen = RandomSentenceGenerator()
-    for txt in args.textfile:
-        gen.feed(txt)
-    print(gen.get_sentence(args.min_words))
+
+    # Restore pickled data
+    if args.load:
+        gen.restore_data(args.load)
+
+    # Read one or more text files
+    if args.textfile:
+        for txt in args.textfile:
+            gen.feed(txt)
+
+    # Print a generated sentence
+    if not args.quiet:
+        print(gen.get_sentence(args.min_words))
+
+    # Pickle the data
+    if args.save:
+        gen.save_data(args.save)
 
 
 if __name__ == '__main__':
